5fc4c4bc8c0e5a381ed58577b159f125c1a00e9b
tjasz <nukorausa@hotmail.com>
2024-10-18 12:07:22 -0700

    Add types for database tables

--- src/types.ts
+++ src/types.ts
@@ -9,9 +9,55 @@ export interface Word {
   Creation: Date;
 }
 
+export interface WordTranslation {
+  Id: number;
+  Source: number;
+  Target: number;
+  Creation: Date;
+}
+
 export interface Phrase {
   Id: number;
   Language: string;
   Spelling: string;
+  Creation: Date;
+}
+
+export interface PhraseMembership {
+  Id: number;
+  Word: number;
+  Phrase: number;
+  Creation: Date;
+}
+
+export interface PhraseTranslation {
+  Id: number;
+  Source: number;
+  Target: number;
+  Creation: Date;
+}
+
+export interface PhraseMembershipTranslation {
+  Id: number;
+  PhraseMembership: number;
+  Word: number;
+  Creation: Date;
+}
+
+export interface Tag {
+  Name: string;
+}
+
+export interface TagWordRelation {
+  Id: number;
+  Tag: string;
+  Word: number;
+  Creation: Date;
+}
+
+export interface TagPhraseRelation {
+  Id: number;
+  Tag: string;
+  Phrase: number;
   Creation: Date;
 }
