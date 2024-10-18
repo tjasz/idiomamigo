32eda3f0c08274e0710e3e70aa6dcc8b600c8f46
tjasz <nukorausa@hotmail.com>
2024-10-17 21:23:53 -0700

    Add database config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,78 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const list = async () => {
+    const endpoint = '/data-api/rest/Person';
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.table(data.value);
+  };
+
+  const get = async () => {
+    const id = 1;
+    const endpoint = `/data-api/rest/Person/Id`;
+    const response = await fetch(`${endpoint}/${id}`);
+    const result = await response.json();
+    console.table(result.value);
+  };
+
+  const update = async () => {
+    const id = 1;
+    const data = {
+      Name: "Molly"
+    };
+
+    const endpoint = '/data-api/rest/Person/Id';
+    const response = await fetch(`${endpoint}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+    console.table(result.value);
+  };
+
+  const create = async () => {
+    const data = {
+      Name: "Pedro"
+    };
+
+    const endpoint = `/data-api/rest/Person/`;
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+    console.table(result.value);
+  };
+
+  const del = async () => {
+    const id = 3;
+    const endpoint = '/data-api/rest/Person/Id';
+    const response = await fetch(`${endpoint}/${id}`, {
+      method: "DELETE"
+    });
+    if (response.ok) {
+      console.log(`Record deleted: ${id}`)
+    } else {
+      console.log(response);
+    }
+  };
+
+  return <div>
+    <h1>Static Web Apps Database Connections</h1>
+    <blockquote>
+      Open the console in the browser developer tools to see the API responses.
+    </blockquote>
+    <div>
+      <button id="list" onClick={list}>List</button>
+      <button id="get" onClick={get}>Get</button>
+      <button id="update" onClick={update}>Update</button>
+      <button id="create" onClick={create}>Create</button>
+      <button id="delete" onClick={del}>Delete</button>
+    </div>
+  </div>;
 }
 
 export default App;
