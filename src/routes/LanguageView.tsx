4990117f5111c63d9dbd263cd9d39fa6767db72f
tjasz <nukorausa@hotmail.com>
2024-10-19 08:11:38 -0700

    Improve the LanguageView a bit

--- src/routes/LanguageView.tsx
+++ src/routes/LanguageView.tsx
@@ -4,25 +4,15 @@ import { useGetLanguageWithWordsQuery } from "../redux-api";
 
 export function LanguageView() {
   const params = useParams();
-  console.log({ params });
   const { Name } = params;
   const { data, error, isLoading } = useGetLanguageWithWordsQuery(Name!, { skip: Name === undefined });
 
-  if (Name === undefined) {
-    console.log("No Name parameter.")
-    return <h1>Language: [undefined]</h1>
-  }
-  if (data === undefined) {
-    console.log("No Language found.")
-    return <h1>Language: [undefined]</h1>
-  }
-
   return <div>
-    <h1>Language: '{data.Name}'</h1>
+    <h1>Language: '{Name}'</h1>
     {isLoading && "..."}
     {error && <span style={{ color: "red" }}>{JSON.stringify(error)}</span>}
     <ul>
-      {data.Words.map(word => <li key={word.Spelling}>{word.Spelling}</li>)}
+      {data && data.Words.map(word => <li key={word.Spelling}>{word.Spelling}</li>)}
     </ul>
   </div>
 }
