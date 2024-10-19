9affef8129d6bbfc7cad279f22d636d9c244931f
tjasz <nukorausa@hotmail.com>
2024-10-18 22:23:47 -0700

    Add breadcrumbs

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Box, Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
-import { Link, Outlet } from 'react-router-dom';
+import { Box, Breadcrumbs, Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 export default function Root() {
+  const location = useLocation();
+  const path = location.pathname === "/" ? [""] : location.pathname.split("/");
+  const breadcrumbs = path.reduce<{ title: string, path: string }[]>(
+    (prev, curr) =>
+      [...prev, (prev.length > 0 ? { title: curr, path: `${prev[prev.length - 1].path}/${curr}` } : { title: "Home", path: curr })],
+    []);
+
   const drawerWidth = 150;
+
   return <div>
     <Drawer open={true} variant="persistent">
       <Box sx={{ width: drawerWidth }} role="presentation">
@@ -21,6 +29,11 @@ export default function Root() {
       </Box>
     </Drawer>
     <div style={{ marginLeft: drawerWidth }}>
+      <Breadcrumbs>
+        {breadcrumbs.map(crumb =>
+          <Link to={crumb.path}>{crumb.title}</Link>
+        )}
+      </Breadcrumbs>
       <Outlet />
     </div>
   </div>
