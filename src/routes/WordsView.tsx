16e42f413573ca20d6c39cd48c30e9cc3a68c248
tjasz <nukorausa@hotmail.com>
2024-10-25 07:58:03 -0700

    Add word creation to word list view

--- src/routes/WordsView.tsx
+++ src/routes/WordsView.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import { useDeleteWordMutation, useListWordsQuery } from "../redux-api";
+import React, { useState } from "react";
+import { useCreateWordMutation, useDeleteWordMutation, useListLanguagesQuery, useListWordsQuery } from "../redux-api";
 import { Link } from "react-router-dom";
+import ApiError from "../ApiError";
+import { LinearProgress } from "@mui/material";
 
 export function WordsView() {
   const { data, error, isLoading } = useListWordsQuery();
 
   // TODO show error if delete fails (may be due to being part of a PhraseMembership or other relation)
   const [deleteWord, { isLoading: isDeletingWord }] = useDeleteWordMutation();
+
+  if (error) {
+    return <ApiError error={error} />
+  }
+
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+
+  if (!data) {
+    return <span style={{ color: "red" }}>Data not defined, even though not loading.</span>;
+  }
 
   return <div>
     <h1>Words</h1>
@@ -18,15 +32,63 @@ export function WordsView() {
           <th>Creation</th>
           <th>DELETE</th>
         </tr>
-        {isLoading && "..."}
-        {error && <span style={{ color: "red" }}>{JSON.stringify(error)}</span>}
-        {data && data.map(word => <tr key={word.Id}>
+        {data.map(word => <tr key={word.Id}>
           <td><Link to={`/Languages/${word.Language}`}>{word.Language}</Link></td>
           <td><Link to={word.Id.toString()}>{word.Spelling}</Link></td>
           <td>{word.Creation.toLocaleString()}</td>
           <td><button disabled={isDeletingWord} onClick={() => deleteWord(word.Id)}><span style={{ color: "red" }}>DELETE</span></button></td>
         </tr>)}
+        <AddWordRow />
       </tbody>
     </table>
   </div>
+}
+
+function AddWordRow() {
+  const { data: languages, error: getLanguageError, isLoading: isLoadingLanguages } = useListLanguagesQuery();
+  const [createWord, { isLoading: isCreatingWord }] = useCreateWordMutation();
+
+  const [language, setLanguage] = useState<string | undefined>(undefined);
+  const [spelling, setSpelling] = useState<string | undefined>(undefined);
+
+  if (getLanguageError) {
+    return <tr><td colSpan={4}><ApiError error={getLanguageError} /></td></tr>
+  }
+
+  if (isLoadingLanguages) {
+    return <tr><td colSpan={4}><LinearProgress /></td></tr>;
+  }
+
+  if (!languages) {
+    return <tr><td colSpan={4}><span style={{ color: "red" }}>Languages list not defined, even though not loading.</span></td></tr>;
+  }
+
+  return <tr>
+    <td>
+      <select onChange={(event) => setLanguage(event.target.value)}>
+        <option value={undefined} disabled selected><em>Select...</em></option>
+        {languages.map(language => <option key={language.Name} value={language.Name}>
+          {language.Name}
+        </option>)}
+      </select>
+    </td>
+    <td>
+      <input id="newWordSpelling" type="text" onChange={(event) => setSpelling(event.target.value)} />
+    </td>
+    <td>
+      <button disabled={isCreatingWord} onClick={() => {
+        if (language === undefined || spelling === undefined) {
+          alert("Language and spelling required to create a word.")
+        } else {
+          createWord({
+            Language: language,
+            Spelling: spelling,
+            Creation: new Date(),
+          });
+          setSpelling(undefined);
+        }
+      }}>Create</button>
+    </td>
+    <td></td>
+  </tr>
 }
