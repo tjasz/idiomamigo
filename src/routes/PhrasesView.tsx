645f6c42e2cf60a39f07cee0f1afcb4abe944f0e
tjasz <nukorausa@hotmail.com>
2024-10-24 21:09:00 -0700

    Split create phrase row from rest of table

--- src/routes/PhrasesView.tsx
+++ src/routes/PhrasesView.tsx
@@ -5,25 +5,17 @@ import ApiError from "../ApiError";
 import { LinearProgress } from "@mui/material";
 
 export function PhrasesView() {
-  const { data: languages, error: getLanguageError, isLoading: isLoadingLanguages } = useListLanguagesQuery();
   const { data: phrases, error: listPhrasesError, isLoading: isLoadingPhrases } = useListPhrasesQuery();
 
-  const [createPhrase, { isLoading: isCreatingPhrase }] = useCreatePhraseMutation();
-  const [language, setLanguage] = useState<string | undefined>(undefined);
-  const [spelling, setSpelling] = useState<string | undefined>(undefined);
-
-  if (getLanguageError) {
-    return <ApiError error={getLanguageError} />
-  }
   if (listPhrasesError) {
     return <ApiError error={listPhrasesError} />
   }
 
-  if (isLoadingLanguages || isLoadingPhrases) {
+  if (isLoadingPhrases) {
     return <LinearProgress />;
   }
 
-  if (!phrases || !languages) {
+  if (!phrases) {
     return <span style={{ color: "red" }}>Data not defined, even though not loading.</span>;
   }
 
@@ -41,33 +33,56 @@ export function PhrasesView() {
           <td><Link to={phrase.Id.toString()}>{phrase.Spelling}</Link></td>
           <td>{phrase.Creation.toLocaleString()}</td>
         </tr>)}
-        <tr>
-          <td>
-            <select onChange={(event) => setLanguage(event.target.value)}>
-              <option value={undefined} disabled selected><em>Select...</em></option>
-              {languages.map(language => <option key={language.Name} value={language.Name}>
-                {language.Name}
-              </option>)}
-            </select>
-          </td>
-          <td>
-            <input id="newPhraseSpelling" type="text" onChange={(event) => setSpelling(event.target.value)} />
-          </td>
-          <td>
-            <button disabled={isCreatingPhrase} onClick={() => {
-              if (language === undefined || spelling === undefined) {
-                alert("Language and spelling required to create a phrase.")
-              } else {
-                createPhrase({
-                  Language: language,
-                  Spelling: spelling,
-                  Creation: new Date(),
-                });
-              }
-            }}>Create</button>
-          </td>
-        </tr>
+        <AddPhraseRow />
       </tbody>
     </table>
   </div>
+}
+
+function AddPhraseRow() {
+  const { data: languages, error: getLanguageError, isLoading: isLoadingLanguages } = useListLanguagesQuery();
+  const [createPhrase, { isLoading: isCreatingPhrase }] = useCreatePhraseMutation();
+
+  const [language, setLanguage] = useState<string | undefined>(undefined);
+  const [spelling, setSpelling] = useState<string | undefined>(undefined);
+
+  if (getLanguageError) {
+    return <tr><td colSpan={3}><ApiError error={getLanguageError} /></td></tr>
+  }
+
+  if (isLoadingLanguages) {
+    return <tr><td colSpan={3}><LinearProgress /></td></tr>;
+  }
+
+  if (!languages) {
+    return <tr><td colSpan={3}><span style={{ color: "red" }}>Languages list not defined, even though not loading.</span></td></tr>;
+  }
+
+  return <tr>
+    <td>
+      <select onChange={(event) => setLanguage(event.target.value)}>
+        <option value={undefined} disabled selected><em>Select...</em></option>
+        {languages.map(language => <option key={language.Name} value={language.Name}>
+          {language.Name}
+        </option>)}
+      </select>
+    </td>
+    <td>
+      <input id="newPhraseSpelling" type="text" onChange={(event) => setSpelling(event.target.value)} />
+    </td>
+    <td>
+      <button disabled={isCreatingPhrase} onClick={() => {
+        if (language === undefined || spelling === undefined) {
+          alert("Language and spelling required to create a phrase.")
+        } else {
+          createPhrase({
+            Language: language,
+            Spelling: spelling,
+            Creation: new Date(),
+          });
+          setSpelling(undefined);
+        }
+      }}>Create</button>
+    </td>
+  </tr>
 }
