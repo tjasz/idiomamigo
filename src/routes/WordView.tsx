6d2b1371eec4913b5d39b27feab4a592f67247c6
tjasz <nukorausa@hotmail.com>
2024-11-04 21:26:26 -0800

    Refactor translation view a bit

--- src/routes/WordView.tsx
+++ src/routes/WordView.tsx
@@ -1,46 +1,30 @@
-import React from "react";
+import React, { FC } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useCreateTagWordRelationMutation, useListTranslationViewsForWordQuery, useGetWordWithPhrasesAndTagsQuery } from "../redux-api";
 import TagDetails from "../TagDetails";
+import ApiError from "../ApiError";
+import { LinearProgress } from "@mui/material";
 
 export function WordView() {
   const params = useParams();
   const { Id } = params;
   const IdAsInt = parseInt(Id ?? "0");
   const { data, error, isLoading } = useGetWordWithPhrasesAndTagsQuery(IdAsInt, { skip: Id === undefined });
-  const { data: translations, error: translationsError, isLoading: translationsIsLoading } = useListTranslationViewsForWordQuery(IdAsInt, { skip: Id === undefined });
   const [attachTag, { isLoading: isAttaching }] = useCreateTagWordRelationMutation();
 
+  if (error) {
+    return <ApiError error={error} />
+  }
+
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+
   return <div>
-    {isLoading && "..."}
-    {error && <span style={{ color: "red" }}>{JSON.stringify(error)}</span>}
     {data && <div>
       <h1>Word: '{data.Spelling}' (<Link to={`/Languages/${data.Language}`}>{data.Language}</Link>)</h1>
       Created: {data.Creation.toLocaleString()}
-      {!!translationsError
-        ? <span style={{ color: "red" }}>{JSON.stringify(translationsError)}</span>
-        : translationsIsLoading
-          ? "..."
-          : <div>
-            <h2>Translations</h2>
-            <ul>
-              {translations!.map(translation => {
-                const otherWord = translation.SourceId === IdAsInt ? {
-                  Id: translation.TargetId,
-                  Language: translation.TargetLanguage,
-                  Spelling: translation.TargetSpelling,
-                } : {
-                  Id: translation.SourceId,
-                  Language: translation.SourceLanguage,
-                  Spelling: translation.SourceSpelling,
-                };
-                return <li key={translation.Id}>
-                  <Link to={`/Words/${otherWord.Id}`}><strong>{otherWord.Language}: </strong>{otherWord.Spelling}</Link>
-                </li>
-              })}
-            </ul>
-          </div>
-      }
+      <TranslationView wordId={IdAsInt} />
       <h2>Phrases:</h2>
       <ul>
         {data.Phrases.map(phrase => <li key={phrase.Id}><Link to={`/Phrases/${phrase.Id}`}>{phrase.Spelling}</Link></li>)}
@@ -52,4 +36,39 @@ export function WordView() {
       })} />
     </div>}
   </div>
+}
+
+interface TranlationViewProps {
+  wordId: number;
+}
+const TranslationView: FC<TranlationViewProps> = ({ wordId }: TranlationViewProps) => {
+  const { data: translations, error: translationsError, isLoading: translationsIsLoading } = useListTranslationViewsForWordQuery(wordId);
+
+  if (translationsError) {
+    return <ApiError error={translationsError} />
+  }
+
+  if (translationsIsLoading) {
+    return <LinearProgress />;
+  }
+
+  return <div>
+    <h2>Translations</h2>
+    <ul>
+      {translations!.map(translation => {
+        const otherWord = translation.SourceId === wordId ? {
+          Id: translation.TargetId,
+          Language: translation.TargetLanguage,
+          Spelling: translation.TargetSpelling,
+        } : {
+          Id: translation.SourceId,
+          Language: translation.SourceLanguage,
+          Spelling: translation.SourceSpelling,
+        };
+        return <li key={translation.Id}>
+          <Link to={`/Words/${otherWord.Id}`}><strong>{otherWord.Language}: </strong>{otherWord.Spelling}</Link>
+        </li>
+      })}
+    </ul>
+  </div>;
 }
